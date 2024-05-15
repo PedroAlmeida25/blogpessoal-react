80608cffd92490ec41fb43d47ac900c0bed5a102
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-05-15 11:48:40 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './paginas/home/Home'
+
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+    {/* <Nav/> */}
+    <Home/>
+    {/* <Footer/> */}
+    </>
+  )
+}
+
+export default App
