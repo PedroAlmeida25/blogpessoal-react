2089d5afe96b7afe61cd2ed9ae37ba7cfac4c85c
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-05-22 16:52:12 -0300

    Blog Atualizado - 22/05

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Login from './paginas/login/Login'
 import axios from 'axios';
 import Cadastro from './paginas/cadastro/Cadastro'
 import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import FormularioTema from './components/temas/formulariotema/FormularioTema'
+import DeletarTema from './components/temas/deletartema/DeletarTema'
 
 
 axios.get('/user?ID=12345')
@@ -29,10 +32,14 @@ function App() {
         <Navbar/>
         <div className="min-h-[80vh]">
         <Routes>
-            <Route path="/" element={<Cadastro/>} />
+            <Route path="/" element={<Login/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/cadastro" element={<Cadastro/>} />
             <Route path="/home" element={<Home/>} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastrartemas" element={<FormularioTema />} />
+            <Route path="/editarTema/:id" element={<FormularioTema />} />
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
         </Routes> 
         </div>
         <Footer/>
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,7 +1,18 @@
-import { Link} from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate} from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
 
-function Navbar() {
-   
+function Navbar() { 
+  let navigate = useNavigate()
+
+  const { handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
   return (
     <>
      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
@@ -12,10 +23,10 @@ function Navbar() {
 
             <div className='flex gap-4'>
               <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
-              <div className='hover:underline'>Cadastrar tema</div>
+              <Link to='/temas' className='hover:underline'>Temas</Link>
+              <Link to='/cadastrartemas' className='hover:underline'>Cadastrar Temas</Link>
               <div className='hover:underline'>Perfil</div>
-              <Link to="/login" className="hover:underline">Sair</Link>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
             </div>
           </div>
         </div>
