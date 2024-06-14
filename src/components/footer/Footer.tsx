da3210bcbce3cae63d5a8919be4702e56e3a2d58
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-06-14 10:17:00 -0300

    fix: footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -17,9 +17,15 @@ function Footer() {
           <p className='text-xl font-bold'>Blog pessoal Pedro Almeida | Copyright: {data} </p>
           <p className='text-lg'>Acesse minhas redes sociais</p>
           <div className='flex gap-2'>
-            <LinkedinLogo size={48} weight='bold' />
-            <InstagramLogo size={48} weight='bold' />
-            <GithubLogo size={48} weight='bold' />
+            <a href="https://www.linkedin.com/in/pedroaugustosantosalmeida/">
+              <LinkedinLogo size={48} weight='bold' />
+            </a>
+            <a href="https://www.instagram.com/pedroalm._/">
+              <InstagramLogo size={48} weight='bold' />
+            </a>
+            <a href="https://github.com/PedroAlmeida25">
+              <GithubLogo size={48} weight='bold' />
+            </a>
           </div>
         </div>
       </div>
