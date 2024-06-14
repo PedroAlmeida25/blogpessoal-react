06050007d596b1c353c5365b1e1cd4830a1eaf96
Pedro Almeida <almeida.pedroaugusto25@gmail.com>
2024-06-14 10:12:42 -0300

    fix: usuario

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -42,7 +42,7 @@ function login(e: ChangeEvent<HTMLFormElement>) {
         <form className="flex justify-center items-center flex-col w-1/2 gap-4" onSubmit={login}>
           <h2 className="text-slate-900 text-5xl ">Entrar</h2>
           <div className="flex flex-col w-full">
-            <label htmlFor="usuario">Usuário</label>
+            <label htmlFor="usuario">Email</label>
             <input
               type="text"
               id="usuario"
